99f8f22ff7cf096b659d9fac54fac575e547756d
Gabriel Barbosa Fernandes de Oliveira <gabriel521ton@gmail.com>
2026-04-23 16:47:38 -0300

    feat: define constantes de tipos de tokens

---
+++ tokens.py
@@ -0,0 +1,28 @@
+# Integrantes do grupo (ordem alfabética):
+# Eduardo Hideo Itinoseke Ogassawara - hiduard
+# Gabriel Barbosa Fernandes de Oliveira - GabrielBarbosaFernandes
+#
+# Nome do grupo no Canvas: RA2 17
+
+LPAREN = "LPAREN"
+RPAREN = "RPAREN"
+
+NUMBER = "NUMBER"
+MEMORY = "MEMORY"
+
+OPERATOR = "OPERATOR"
+REL_OP = "REL_OP"
+
+RES = "RES"
+
+START = "START"
+END = "END"
+
+IF = "IF"
+ELSE = "ELSE"
+ENDIF = "ENDIF"
+
+WHILE = "WHILE"
+ENDWHILE = "ENDWHILE"
+
+INVALID = "INVALID"
