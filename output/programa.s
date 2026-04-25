72a9dc66e181fea95ff48c45acced33d95ed0c95
Eduardo <eduogassa@gmail.com>
2026-04-24 21:52:07 -0300

    feat/assemby atualizac do cpulator

--- output/programa.s
+++ output/programa.s
@@ -40,6 +40,23 @@ RESULTADO_19: .double 0.0
 RESULTADO_20: .double 0.0
 RESULTADO_21: .double 0.0
 
+@ Strings para impressao no JTAG UART
+STR_DOBRO: .asciz "DOBRO="
+STR_MEDIA: .asciz "MEDIA="
+STR_PI: .asciz "PI="
+STR_POT: .asciz "POT="
+STR_QUOC: .asciz "QUOC="
+STR_RESTO: .asciz "RESTO="
+STR_SAIDA: .asciz "SAIDA="
+STR_SOMA: .asciz "SOMA="
+STR_ULTIMO: .asciz "ULTIMO="
+STR_X: .asciz "X="
+STR_Y: .asciz "Y="
+STR_NL: .asciz "\n"
+STR_PONTO: .asciz "."
+STR_MENOS: .asciz "-"
+PRINT_BUF: .space 12
+
 .text
 .global _start
 _start:
@@ -298,6 +315,202 @@ WHILE_INICIO_18_15:
     vstr d0, [r0]
     b WHILE_INICIO_18_15
 WHILE_FIM_18_16:
+
+@ ============================================================
+@ Impressao dos resultados no JTAG UART
+@ ============================================================
+    @ Imprime DOBRO
+    ldr r1, =STR_DOBRO
+    bl print_str
+    ldr r0, =MEM_DOBRO
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime MEDIA
+    ldr r1, =STR_MEDIA
+    bl print_str
+    ldr r0, =MEM_MEDIA
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime PI
+    ldr r1, =STR_PI
+    bl print_str
+    ldr r0, =MEM_PI
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime POT
+    ldr r1, =STR_POT
+    bl print_str
+    ldr r0, =MEM_POT
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime QUOC
+    ldr r1, =STR_QUOC
+    bl print_str
+    ldr r0, =MEM_QUOC
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime RESTO
+    ldr r1, =STR_RESTO
+    bl print_str
+    ldr r0, =MEM_RESTO
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime SAIDA
+    ldr r1, =STR_SAIDA
+    bl print_str
+    ldr r0, =MEM_SAIDA
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime SOMA
+    ldr r1, =STR_SOMA
+    bl print_str
+    ldr r0, =MEM_SOMA
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime ULTIMO
+    ldr r1, =STR_ULTIMO
+    bl print_str
+    ldr r0, =MEM_ULTIMO
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime X
+    ldr r1, =STR_X
+    bl print_str
+    ldr r0, =MEM_X
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+    @ Imprime Y
+    ldr r1, =STR_Y
+    bl print_str
+    ldr r0, =MEM_Y
+    vldr d0, [r0]
+    bl print_double
+    ldr r1, =STR_NL
+    bl print_str
+
+    b fim
+
+@ ============================================================
+@ Subrotinas de impressao no JTAG UART (0xFF201000)
+@ Polling correto: verifica espaco na FIFO de escrita antes
+@ ============================================================
+
+@ print_char: imprime caractere em r2 (espera FIFO ter espaco)
+print_char:
+    push {r0, r3, lr}
+    ldr r0, =0xFF201000
+pc_wait:
+    ldr r3, [r0, #4]
+    lsr r3, r3, #16
+    cmp r3, #0
+    beq pc_wait
+    str r2, [r0]
+    pop {r0, r3, lr}
+    bx lr
+
+@ print_str: imprime string em r1
+print_str:
+    push {r2, lr}
+ps_loop:
+    ldrb r2, [r1], #1
+    cmp r2, #0
+    beq ps_ret
+    bl print_char
+    b ps_loop
+ps_ret:
+    pop {r2, lr}
+    bx lr
+
+@ print_int: imprime inteiro em r1
+print_int:
+    push {r4, r5, r6, r7, lr}
+    mov r4, r1
+    ldr r6, =PRINT_BUF
+    add r6, r6, #11
+    mov r7, #0
+    strb r7, [r6]
+    cmp r4, #0
+    bge pi_conv
+    ldr r1, =STR_MENOS
+    bl print_str
+    rsb r4, r4, #0
+pi_conv:
+    sub r6, r6, #1
+    mov r7, r4
+    mov r4, #0
+pi_d10:
+    cmp r7, #10
+    blt pi_d10d
+    sub r7, r7, #10
+    add r4, r4, #1
+    b pi_d10
+pi_d10d:
+    add r7, r7, #48
+    strb r7, [r6]
+    cmp r4, #0
+    bne pi_conv
+pi_imp:
+    ldrb r2, [r6], #1
+    cmp r2, #0
+    beq pi_fim
+    bl print_char
+    b pi_imp
+pi_fim:
+    pop {r4, r5, r6, r7, lr}
+    bx lr
+
+@ print_double: imprime double em d0 com 2 casas decimais
+print_double:
+    push {r4, r5, lr}
+    vmov r4, r5, d0
+    tst r5, #0x80000000
+    beq pd_pos
+    ldr r1, =STR_MENOS
+    bl print_str
+    vneg.f64 d0, d0
+pd_pos:
+    vcvt.s32.f64 s0, d0
+    vmov r1, s0
+    bl print_int
+    ldr r1, =STR_PONTO
+    bl print_str
+    vcvt.f64.s32 d1, s0
+    vsub.f64 d0, d0, d1
+    ldr r4, =CONST_10_0
+    vldr d2, [r4]
+    vmul.f64 d0, d0, d2
+    vmul.f64 d0, d0, d2
+    vcvt.s32.f64 s0, d0
+    vmov r1, s0
+    cmp r1, #10
+    bge pd_2d
+    mov r2, #48
+    bl print_char
+pd_2d:
+    bl print_int
+    pop {r4, r5, lr}
+    bx lr
+
 
 fim:
     b fim
